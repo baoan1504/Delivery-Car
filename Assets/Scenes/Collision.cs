2e76db9ad36272218859a856aadd07a2f6ac2060
baoan1504 <rahampazzedkd2507@gmail.com>
2025-06-26 14:14:29 +0700

    GetComponent thay đổi màu xe

--- Assets/Scenes/Collision.cs
+++ Assets/Scenes/Collision.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class Collsions : MonoBehaviour
 {
-
     bool hasPackage = false;
     [SerializeField] float DestroyDelay = 0.5f;
-    
+    [SerializeField] Color32 PackageColor = new Color32(255, 1, 1, 255);
+    [SerializeField] Color32 noPackageColor = new Color32(255, 255, 255, 255);
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Package" && !hasPackage)
         {
             Debug.Log("oh gói hàng");
             hasPackage = true;
+            spriteRenderer.color = PackageColor;
             Destroy(other.gameObject, DestroyDelay);
         }
         if (other.tag == "Location" && hasPackage)
         {
             Debug.Log("I receive package");
             hasPackage = false;
+            spriteRenderer.color = noPackageColor;
         }
     }
 }
