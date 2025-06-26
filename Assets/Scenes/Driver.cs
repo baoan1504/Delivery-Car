31d44250b995fad3a261183ef93f75ce8a665198
baoan1504 <rahampazzedkd2507@gmail.com>
2025-06-26 14:26:58 +0700

    tăng tốc & giảm tốc xe

--- Assets/Scenes/Driver.cs
+++ Assets/Scenes/Driver.cs
@@ -6,20 +6,36 @@ public class Driver : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 0.1f;
 
-    [SerializeField] float steerSpeed = 0.2f; 
+    [SerializeField] float steerSpeed = 0.2f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float normalSpeed = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         // trả về các giá trị từ khoảng -1 đến 1, và sẽ 0 khi không có tác động
-        float changeSteer = Input.GetAxis("Horizontal") *steerSpeed *Time.deltaTime;
-        float changeMove = Input.GetAxis("Vertical") * moveSpeed *Time.deltaTime;
+        float changeSteer = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float changeMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Translate(0, changeMove, 0);
         transform.Rotate(0, 0, -changeSteer);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        moveSpeed = normalSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
